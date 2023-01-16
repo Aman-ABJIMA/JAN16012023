a4e186f5da826c0382e4a2dd48c5bfcb218f96ba
Aman-ABJIMA <aman.singh@abjima.com>
2023-01-16 18:56:26 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <center>
+    <form id="form1" runat="server">
+        <h3>LIST of EMPLOYEE</h3>
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" BorderColor="#507CD1" ForeColor="#507CD1" Width="205px">  </asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" BackColor="#507CD1" ForeColor="White" Text="search" />
+            <br />
+        </div>
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Height="338px" Width="1079px">
+            <AlternatingRowStyle BackColor="White" />
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+    </form>
+        </center>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // string connection_string = "server=.;database=MYDATABASE;integrated security=SSPI";
+            string connection_string=ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            #region SQL command class
+            
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            { 
+
+
+                SqlCommand command2 = new SqlCommand("SELECT COUNT(NAME) FROM EMPLOYEE", connection);
+                connection.Open();
+                int TOTAL_ROWS=(int)command2.ExecuteScalar();
+                Response.Write("Total Rows=" + TOTAL_ROWS.ToString());
+
+
+            }
+
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command3 = new SqlCommand("INSERT INTO EMPLOYEE VALUES(null,'ANISH','JAIPUR','MALE',123456789,'NOT ALLOWED')",connection);
+                connection.Open();
+                int TOTAL_ROWS_AFFACTED=command3.ExecuteNonQuery();
+                Response.Write("\nTotal Rows Affacted=" + TOTAL_ROWS_AFFACTED);
+            }
+
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command4 = new SqlCommand("UPDATE EMPLOYEE SET ID=4 WHERE NAME='ANISH'",connection);
+                connection.Open();
+                command4.ExecuteNonQuery();
+                Response.Write("\nUpdated");
+            }
+
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command5 = new SqlCommand("DELETE EMPLOYEE WHERE NAME='ANISH'", connection);
+                connection.Open();
+                command5.ExecuteNonQuery();
+                Response.Write("\nDeleted");
+            }
+
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command = new SqlCommand("SELECT *FROM Employee", connection);
+                connection.Open();
+                //SqlDataReader reader = command.ExecuteReader();
+                //GridView1.DataSource = reader;
+                GridView1.DataSource = command.ExecuteReader();
+                GridView1.DataBind();
+            }
+            
+            #endregion
+
+
+
+
+        }
+
+           
+
+
+
+
+    }
+
+}
